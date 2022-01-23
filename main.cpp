4d3912f1c56890b5c324ec49d4c3b13c0db650a5
Portoky <56086483+Portoky@users.noreply.github.com>
2022-01-23 12:01:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,61 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <SDL_ttf.h> //eddig az alap dolgok vannak incudeolva, talan nehany folosben van (a modularis programozas miatt), checkold kesobb
-#include "mainwindow.h" //ez az ablak generator header fajlja (crtl + click <3)
+#include "../SDL2/include/SDL.h"
+#include "../SDL2/SDL2_Image/include/SDL_image.h"
+#include "../SDL2/SDL2_mixer/include/SDL_mixer.h"
+#include "../SDL2/SDL2_ttf/include/SDL_ttf.h"
 #include "functions.h"
+#include "Classes.h"
+
+
 
 #undef main
 
+extern SDL_Window* Window;
+extern SDL_Renderer* Renderer;
+extern LButtonPosition buttons[5];
+
+
+
 int main()
 {
-	//mainwindow();
-	init();
+	if (init())
+	{
+		if (loadMedia())
+		{
+
+			bool quit = false;
+			SDL_Event e;
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+
+					for (int i = 0; i < 5; ++i)
+					{
+						if (buttons[i].active)
+						{
+							buttons[i].HandleEvent(&e);
+						}
+					}
+
+				}
+				///Main Menu
+				for (int i = 1; i <= 2; ++i)
+					buttons[i].active = true;
+
+
+
+
+				SDL_SetRenderDrawColor(Renderer, 121, 85, 72, 255);
+				SDL_RenderClear(Renderer);
+				SDL_RenderPresent(Renderer);
+			}
+		}
+	}
+
+	
 }
