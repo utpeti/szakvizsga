f3cdc76a356eab80b0dc3374d13ae0558e34bb61
Portoky <56086483+Portoky@users.noreply.github.com>
2022-01-23 12:00:44 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,46 +1,49 @@
-#ifndef functions_h
-#define functions_h
-
-
 #include <iostream>
-#include <SDL.h>
+#include "../SDL2/include/SDL.h"
+#include "../SDL2/SDL2_Image/include/SDL_image.h"
+#include "../SDL2/SDL2_mixer/include/SDL_mixer.h"
+#include "../SDL2/SDL2_ttf/include/SDL_ttf.h"
+/*#include <SDL.h>
 #include <SDL_image.h>
-#include <SDL_mixer.h>
 #include <SDL_ttf.h>
+#include <SDL_mixer.h>*/
+#include "Classes.h"
 
-using namespace std;
+#pragma once
 
-SDL_Window* Window = 0;
-SDL_Renderer* Renderer = 0;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 
-const int WIDTH = 1280;
-const int HEIGHT = 720;
+SDL_Window* Window = NULL;
+SDL_Renderer* Renderer = NULL;
+LBackground kastely; 
+LButtonPosition buttons[5];
 
 bool init()
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        cout << SDL_GetError();
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
 
     if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
     {
-        cout << SDL_GetError();
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
 
-    Window = SDL_CreateWindow("Szakvizsga", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    Window = SDL_CreateWindow("Szakvizsga", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (Window == NULL)
     {
-        cout << SDL_GetError();
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
 
-    SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (Renderer == NULL)
     {
-        cout << SDL_GetError();
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
 
@@ -49,19 +52,35 @@ bool init()
     int imgFlags = IMG_INIT_PNG;
     if (!(IMG_Init(imgFlags) & imgFlags))
     {
-        cout << SDL_GetError();
+        std::cout << IMG_GetError() << "\n";
         return false;
     }
 
     //initialize sdl mixer
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) < 0)    //sound frequency(44100-standard), sample format
-    {                                                            //2 channels (stereo), chunksize (4096 for music) (minden hangnak a merete byte-ban: tul kicsi-kimarad, tul nagy-laggol)
-        cout << SDL_GetError();
+    {
+                                                            //2 channels (stereo), chunksize (4096 for music) (minden hangnak a merete byte-ban: tul kicsi-kimarad, tul nagy-laggol)
+        std::cout << Mix_GetError();
         return false;
     }
 
     return true;
-
+   
 }
 
-#endif
+bool loadMedia()
+{
+    if (!kastely.loadFromFile("Images/Kastely.png"))
+    {
+        std::cout << IMG_GetError() << std::endl;
+        return false;
+    }
+    return true;
+
+    buttons[1].setPosition(SCREEN_HEIGHT / 2 - BUTTON_HEIGHT, SCREEN_WIDTH / 2 - BUTTON_WIDTH);
+}
+
+void stage1()
+{
+    kastely.render(0, 0, NULL);
+}
